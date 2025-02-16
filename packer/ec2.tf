3ab560dae6867903b186b0b412c7f3826e5dd4c9
nazikmusa <musaevan331@gmail.com>
2025-02-16 21:11:31 +0000

    ec2 creating

--- packer/ec2.tf
+++ packer/ec2.tf
@@ -1,28 +1,28 @@
-# # Data source to find the AMI by name or tag
-# data "aws_ami" "wordpress" {
-#   most_recent = true
-#   owners      = ["self"]  # Use "self" if the AMI was created in your AWS account
+# Data source to find the AMI by name or tag
+data "aws_ami" "wordpress" {
+  most_recent = true
+  owners      = ["self"]  # Use "self" if the AMI was created in your AWS account
 
-#   # Filters to match the AMI by name or any other tags you might use
-#   filter {
-#     name   = "name"
-#     values = ["wordpress-ami"]  # Replace with the prefix of your Packer AMI name
-#   }
-# } 
+  # Filters to match the AMI by name or any other tags you might use
+  filter {
+    name   = "name"
+    values = ["WordPress-AMI"]  # Replace with the prefix of your Packer AMI name
+  }
+} 
 
 
-# resource "aws_instance" "group-2" {
-#   ami           = data.aws_ami.wordpress.id
-#   instance_type = var.instance_type
-#   subnet_id = aws_subnet.main.id
-#   key_name = aws_key_pair.deployer.key_name
-#   vpc_security_group_ids = [aws_security_group.allow_tls.id]
+resource "aws_instance" "group-2" {
+  ami           = data.aws_ami.wordpress.id
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.main.id
+  key_name = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.group-2.id]
 
-#   tags = {
-#     Name = var.instance_name
-#   }
+  tags = {
+    Name = var.instance_name
+  }
 
-#   volume_tags = {
-#     backup = "True"
-#   }
-# }
+  volume_tags = {
+    backup = "True"
+  }
+}
--- packer/provider.tf
+++ packer/provider.tf
@@ -1,8 +1,8 @@
-provider "aws" {
-    region = var.region
-}
+# provider "aws" {
+#     region = var.region
+# }
 
-resource "aws_key_pair" "deployer" {
-    key_name = var.key_name
-    public_key = file("~/.ssh/id_rsa.pub")
-}
+# resource "aws_key_pair" "deployer" {
+#     key_name = var.key_name
+#     public_key = file("~/.ssh/id_rsa.pub")
+# }
